d383b174078947f7eb17dae642dc319237359885
Dan Morrell <dan_morrell@msn.com>
2020-02-09 16:05:14 -0600

    Initial Commit

---
+++ script.py
@@ -0,0 +1,19 @@
+import math
+import sys
+
+import requests
+
+from os import rename
+
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
